671463561122cc7b30dd563c396ff4224ddeb5a6
danielware <waredanielb@gmail.com>
2022-06-06 16:35:35 -0700

    Final project
    Co-authored by: Derrak <derrak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+  $("#form").submit(function(event){
+    event.preventDefault();
 
+    const fav1 = $("#favorites1").val();
+    const words = fav1.split(' ');
+    const result = words.filter(word =>word.length >= 3);
+    const reversed = result.reverse();
+    let joinedUp = reversed.join(" ");
+
+    
+
+    console.log(joinedUp);
+
+
+    $("p").append(joinedUp);
+
+  });
 }); 
 
+
+// The quick brown fox jumps over the lazy dog
